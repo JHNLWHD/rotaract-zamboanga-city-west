0137fa65d9e2bd8bd17e25ba9def479336100bee
JHNLWHD <aljhoenilw@gmail.com>
2025-05-16 20:40:35 +0800

    feat: add posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    options={{
+      api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      debug: import.meta.env.MODE === "development",
+    }}
+  >
+    <App />
+  </PostHogProvider>
+);
